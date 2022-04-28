4c204d9e443520b7be257dc7e234d49d9e2bcde4
Gerald Diaz <gerald.diaz11715@est.uca.edu.ni>
2022-04-28 13:04:27 -0600

    Persistencia ROOM

---
+++ app/src/main/java/ni/edu/uca/persistenciaroom/MainActivity.kt
@@ -0,0 +1,30 @@
+package ni.edu.uca.persistenciaroom
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import ni.edu.uca.persistenciaroom.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    /*private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBarWithNavController(findNavController(R.id.listaFragment)
+        )
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.listaFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }*/
+
+    private lateinit var  binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
